b19b3654556044225a65de3f5beeacc66e8dca79
Jason Greenberg <jay.greenberg@elastic.co>
2018-05-16 11:53:50 -0400

    CI script tweak

--- spec/inputs/check_test.sh
+++ spec/inputs/check_test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Simulates the output of /usr/local/sbin/check_load -w 0,2,3 -c 0,5,6
 echo "CRITICAL - load average: 3.42, 0.00, 0.00|load1=3.420;0.000;0.000;0; load5=0.000;2.000;5.000;0; load15=0.000;3.000;6.000;0;"
 exit 2
